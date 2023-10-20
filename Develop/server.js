378842960f3e0b306b5a86c02ef18b7068785ea0
dmod129 <derek.modugno@gmail.com>
2023-10-20 16:56:07 -0500

    added constants and app.use's

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Sets up static middleware
+app.use(express.static('public'));
+// Used to parse incoming requests with JSON payloads
+app.use(express.json());
+// Handles form data submitted in HTML forms
+app.use(express.urlencoded({extendended: true}));
+
+// Routes
+// const apiRoutes =
+
+// const htmlRoutes = 
+
+// Server listener
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
